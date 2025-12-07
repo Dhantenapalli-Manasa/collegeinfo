f3ca8bc52c77e69d1e8c48e6012681d38f31aecb
Dhantenapalli Manasa <manasadhantenapalli@gmail.com>
2025-12-07 12:37:46 +0530

    changes made by manasa

--- collegesinfo/teachersinfo/models.py
+++ collegesinfo/teachersinfo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+class login(models.Model):
+    username: models.TextField(max_length=100)
+    password:models.TextField()
+    def ___str___(self):
+        return self.username
 
 # Create your models here.
