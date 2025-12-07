95b59910f6004683fabc85e7c53fbbd44b776f14
vaishnavi essampelly <vaishnaviessampelly24@gmail.com>
2025-12-07 11:59:14 +0530

    added vaishnavi feature

--- collegesinfo/teachersinfo/views.py
+++ collegesinfo/teachersinfo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-def home(request):
-    return render(request,'landingpage.html')
 
-# Create9 your views here.
+def home(request):
+    return render(request, 'landingpage.html')
+
+def login_page(request):
+    return render(request, 'login.html')
